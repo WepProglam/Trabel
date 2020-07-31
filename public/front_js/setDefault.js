b1567440357b41e9b4ac0b7c0e0e8fe1c1b56938
To-ToRo <ghkdcks1223@gmail.com>
2020-07-31 17:20:38 +0900

    set default place(region) with the cookie

---
+++ public/front_js/setDefault.js
@@ -0,0 +1,20 @@
+function defaultPlace(name) {
+    $('.search_place').val(name);
+}
+
+getCookie = function(name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    
+    for (i = 0; i < ARRcookies.length; i++) {     
+            x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+            y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+            
+            x = x.replace(/^\s+|\s+$/g, "");
+
+            if (x == name) {
+                    return unescape(y);
+            }
+    }
+}
+
+let place = getCookie('place');
